560976d1b88e8e58571c94da39c10baccbc3284d
Asim Abdelgadir <acsaasim@hotmail.com>
2017-02-24 01:33:10 -0500

    webpack configured with code spliting

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,31 +2,46 @@ import webpack from 'webpack';
 import path from 'path';
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
+//our app third party dependencies
+const VENDOR_LIBS = [
+  'babel-polyfill', 'bootstrap', 'jquery', 'react', 'react-dom', 'react-redux', 'react-router', 'react-router-redux',
+  'redux', 'redux-thunk', 'toastr'
+];
+
 export default {
   debug: true,
   devtool: 'inline-source-map',
   noInfo: false,
-  entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
-    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: VENDOR_LIBS,
+    bundle: [path.resolve(__dirname, 'src/index'), 'webpack-hot-middleware/client?reload=true', 'eventsource-polyfill']
+  },
   target: 'web',
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name][hash].js'
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'src')
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new htmlWebpackPlugin({
-      template: 'src/index.html' //if we do not specifiy a template, it will use the default one
-    })
-  ],
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin(),
+      new htmlWebpackPlugin({
+        template: 'src/index.html' //if we do not specifiy a template, it will use the default one
+      }),
+      //this plugin will make sure there is no duplicate modules between vendor.js and bundle.js if there are it will remove them from bundle.js and put them in vendor.js
+      // Extract all 3rd-party modules into a separate chunk
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest']
+      }),
+      //without this you will get: Uncaught ReferenceError: jQuery is not defined
+      new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+      })
+    ],
   module: {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
